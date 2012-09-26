3ca42beeed87632a71a8e5b47ac8d686708c00d9
yellow-sky <nikulin.e@gmail.com>
2012-09-26 16:34:33 +0400

    Ad script for update translations and base file

---
+++ src/i18n/base.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>FieldPyculator</name>
+    <message>
+        <location filename="fieldpyculatorplugin.py" line="39"/>
+        <source>Field pyculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatorplugin.py" line="58"/>
+        <source>&amp;Field pyculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FieldPyculatorDialog</name>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="127"/>
+        <source>FieldPyculator warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="127"/>
+        <source>Layer is not in edit mode! Please start editing the layer!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="306"/>
+        <source>FieldPyculator code execute error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="141"/>
+        <source>Global code block can&apos;t be executed!<byte value="xd"/>%1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="176"/>
+        <source>Field code block can&apos;t be executed!<byte value="xd"/>%1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="288"/>
+        <source>Field code block can&apos;t be executed for feature %3!<byte value="xd"/>%1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="297"/>
+        <source>Field code block does not return &apos;%1&apos; variable! Please declare this variable in your code!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="306"/>
+        <source>Result value can&apos;t be assigned to the feature %3!<byte value="xd"/>%1: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="326"/>
+        <source>FieldPyculator code executed successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fieldpyculatordialog.py" line="326"/>
+        <source>Updated %1 features for %2 seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="21"/>
+        <source>FieldPyculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="28"/>
+        <source>Field expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="49"/>
+        <source>Run</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="58"/>
+        <source>Update field:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="65"/>
+        <source>Global expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="82"/>
+        <source>Layer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="86"/>
+        <source>Update only selected features</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="99"/>
+        <source>LayerName</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="103"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="108"/>
+        <source>Existing fields and vars</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="114"/>
+        <source>Get all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="123"/>
+        <source>$id</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="132"/>
+        <source>Fields</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="139"/>
+        <source>Values</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui_fieldpyculatordialog.py" line="148"/>
+        <source>$geom</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
