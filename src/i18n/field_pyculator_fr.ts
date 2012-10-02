aa8a7ad65c89cc68228622c43aa541780b1648b4
yellow-sky <nikulin.e@gmail.com>
2012-10-02 17:13:39 +0400

    Added french translation

---
+++ src/i18n/field_pyculator_fr.ts
@@ -0,0 +1,155 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>FieldPyculatorDialog</name>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="126"/>
+        <source>FieldPyculator warning</source>
+        <translation>Avertissement</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="126"/>
+        <source>Layer is not in edit mode! Please start editing the layer!</source>
+        <translation>La couche n&apos;est pas en mode édition ! SVP, mettre en mde édition !</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="305"/>
+        <source>FieldPyculator code execute error</source>
+        <translation>
+FieldPyculator est en erreur</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="296"/>
+        <source>Field code block does not return &apos;%1&apos; variable! Please declare this variable in your code!</source>
+        <translation>
+Bloc de code de champ ne retourne pas &apos;%1&apos; variable! S&apos;il vous plaît déclarer cette variable dans votre code!</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="323"/>
+        <source>FieldPyculator code executed successfully</source>
+        <translation>Code FieldPyculator exécuté avec succès</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="323"/>
+        <source>Updated %1 features for %2 seconds</source>
+        <translation>Mise à jour%1 Caractéristiques de%2 secondes</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="21"/>
+        <source>FieldPyculator</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="28"/>
+        <source>Field expression</source>
+        <translation>Expression</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="49"/>
+        <source>Run</source>
+        <translation>Lancer</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="58"/>
+        <source>Update field:</source>
+        <translation>Mettre à jour le champ :</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="65"/>
+        <source>Global expression</source>
+        <translation>Expression globale</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="82"/>
+        <source>Layer:</source>
+        <translation type="unfinished">Couche :</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="86"/>
+        <source>Update only selected features</source>
+        <translation>Mettre à jour uniquement les objets sélectionnés</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="99"/>
+        <source>LayerName</source>
+        <translation>Nom du champ</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="103"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="108"/>
+        <source>Existing fields and vars</source>
+        <translation>Variables et champs existants</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="114"/>
+        <source>Get all</source>
+        <translation>Tout</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="123"/>
+        <source>$id</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="132"/>
+        <source>Fields</source>
+        <translation>Champs</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="139"/>
+        <source>Values</source>
+        <translation>Valeurs</translation>
+    </message>
+    <message>
+        <location filename="ui_field_pyculator_dialog.py" line="148"/>
+        <source>$geom</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="140"/>
+        <source>Global code block can&apos;t be executed!
+%1: %2</source>
+        <translation>Bloc de code global ne peut pas être exécuté !
+%1: %2</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="175"/>
+        <source>Field code block can&apos;t be executed!
+%1: %2</source>
+        <translation>Bloc de code champ ne peut pas être exécuté !
+%1: %2</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="287"/>
+        <source>Field code block can&apos;t be executed for feature %3!
+%1: %2</source>
+        <translation>Bloc de code ne peut pas être exécuté pour l&apos;objet %3!
+%1: %2</translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_dialog.py" line="305"/>
+        <source>Result value can&apos;t be assigned to the feature %3!
+%1: %2</source>
+        <translation>Valeur du résultat ne peut pas être affecté à la fonction de %3!
+%1: %2</translation>
+    </message>
+</context>
+<context>
+    <name>FieldPyculatorPlugin</name>
+    <message>
+        <location filename="field_pyculator_plugin.py" line="57"/>
+        <source>FieldPyculator</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="field_pyculator_plugin.py" line="74"/>
+        <source>&amp;FieldPyculator</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
